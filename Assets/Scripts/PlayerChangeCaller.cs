3bb78ac76f8511154b8fedf571f8911b5cc98a4d
Cristhofer <cristhofermarquescarvalho@gmail.com>
2020-11-10 23:57:18 -0300

    added the shit camera controller

---
+++ Assets/Scripts/PlayerChangeCaller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChangeCaller : MonoBehaviour
+{
+    public CameraMotion camera;
+
+    public delegate void JustCall();
+
+    public event JustCall moved;
+
+    void Start()
+    {
+        camera = FindObjectOfType<CameraMotion>();
+
+        if(camera != null)
+        {
+            moved += camera.RefreshCameraPosition;
+        }
+    }
+    void Update()
+    {
+        transform.position += new Vector3(0.01f,0,0);
+
+        if(moved != null && transform.hasChanged)
+        {
+            moved();
+        }
+    }
+}
