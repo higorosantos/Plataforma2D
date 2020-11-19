eccf0e8cba100f5a831b7560829c8259d8362035
higorosantos <45833012+higorosantos@users.noreply.github.com>
2020-11-19 15:15:35 -0300

    Movimentação Basica
    Movimentação basica , colisões adcionadas

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    ///
+
+    ///
+    [SerializeField] private float radiusRange = 6f;
+    [SerializeField] private float rangeHitEnemy = 3f;
+    private Quaternion rotationEnemy;
+    [SerializeField] private bool hitPlayer;
+    [SerializeField] private bool followPlayer;
+    private bool waitFollow = false;
+    public float timer = 3f;
+    [SerializeField] private Vector2 lastPositionPlayer;
+    [SerializeField] private LayerMask mask;
+    [SerializeField] private Transform respawn;
+
+
+
+
+
+    public Transform player;
+    public Vector2 enemy;
+
+
+    ///Componentes
+    Rigidbody2D rb;
+
+    ///
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        EnemyRunTarget();
+        RandomWalk();
+    }
+
+   
+    private void EnemyRunTarget()
+    {
+
+        //hitPlayer = Physics2D.Raycast(transform.position, -transform.right, rangeHitEnemy, mask);
+        followPlayer = Physics2D.OverlapCircle(transform.position, radiusRange, mask);
+    }
+    private void RandomWalk()
+    {
+        if (followPlayer)
+        {
+            lastPositionPlayer = player.position;
+            Vector2 cu = new Vector2(player.position.x > 0 ? 1 : -1, rb.velocity.y).normalized ;
+            rb.velocity = cu;
+            Debug.Log(cu.x);
+        }
+        
+
+    }
+
+}
---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Ground Checker")]
+    [SerializeField]Transform groundChecker;
+    [SerializeField]float radius;
+    [SerializeField] LayerMask layer;
+    [SerializeField] bool isGround;
+
+
+    Rigidbody2D rg;
+    BoxCollider2D bc;
+    SpriteRenderer sr;
+    
+
+    [Header("Status")]
+
+    [Range(0,15)]
+    [SerializeField]float walkSpeed;
+    [Range(16, 20)]
+    [SerializeField]float runSpeed = 16;
+    //[Range(0, 5)]
+    [SerializeField]float jumpForce;
+    
+
+    float horizontal;
+    Vector2 direction;
+    void Awake()
+    {
+        rg = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+        
+    }
+    
+    
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        Flip();
+        Jump();
+        
+    }
+
+    void FixedUpdate()
+    {
+        Walk();
+        GroundChecker();
+    }
+    void Walk() {
+        direction = new Vector2(horizontal * walkSpeed, rg.velocity.y);
+        rg.velocity = direction;
+        //Debug.Log("Direção sem normalize: " + direction);
+        //Debug.Log(direction.x);
+    }
+
+    void Flip() {
+
+        if(direction.x < 0 && !sr.flipX || direction.x > 0 && sr.flipX)
+        {
+            sr.flipX = !sr.flipX;
+        }
+
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rg.velocity = new Vector2(rg.velocity.x,0);
+            rg.AddForce(new Vector2 (0,jumpForce),ForceMode2D.Impulse);
+            //Debug.Log("JUMP");
+        }
+    }
+    void GroundChecker()
+    {
+        isGround = Physics2D.OverlapCircle(groundChecker.position,radius, layer); 
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //Gizmos.DrawWireSphere(groundChecker.position, radius);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+}
